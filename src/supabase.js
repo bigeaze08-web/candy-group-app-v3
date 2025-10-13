736eed26522027fbccc084885f28c8236c5268e1
bigeaze08-web <bigeaze08@gmail.com>
2025-10-13 18:08:59 +1300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -5,4 +5,5 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 export default supabase
-
+-- A
+create publication supabase_realtime for table public.messages;
