8f174ee415fe59f80b8e79a4383cb753ca17b12a
bigeaze08-web <bigeaze08@gmail.com>
2025-10-13 21:58:54 +1300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // optional; remove if you like
-  server: { port: 5173 },
 })
+
 
 
 
