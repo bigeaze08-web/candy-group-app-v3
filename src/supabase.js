18c7b09e84a580ef5c7776467d19116d4c2ab62f
bigeaze08-web <bigeaze08@gmail.com>
2025-10-14 10:25:57 +1300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const url = import.meta.env.VITE_SUPABASE_URL
+const key = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-export default supabase
--- A
-create publication supabase_realtime for table public.messages;
+export default createClient(url, key)
