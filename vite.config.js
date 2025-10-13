ebdff86bebb28cd8d3ae295cb791653aa1ea7eef
bigeaze08-web <bigeaze08@gmail.com>
2025-10-13 21:27:08 +1300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // optional; remove if you like
+  server: { port: 5173 },
 })
-supabase.storage.from('photos')  // 'photos' must match the bucket you just created
+
 
 
