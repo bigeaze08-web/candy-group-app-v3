27779037a8ebbe5be32bb41485d2963f59835d69
bigeaze08-web <bigeaze08@gmail.com>
2025-10-16 20:04:57 +1300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Keep this file simple: don't import Supabase or read env here
 export default defineConfig({
   plugins: [react()],
 })
